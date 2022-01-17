b0e9db5f7849755a33fe52e5e8a81a3a4bc48b67
supriya-divvela <81727984+supriya-divvela@users.noreply.github.com>
2022-01-17 15:54:42 +0530

    Update tictac_by_2_individuals.py

--- tictac_by_2_individuals.py
+++ tictac_by_2_individuals.py
@@ -8,8 +8,14 @@ while(m=='y'):
     m=input("Enter Who will play first:(X/O):")
     while ' ' in l:
         p=int(input(f"Enter in which position u want to place {m}:"))
-        if l[p-1]==' ':l[p-1]=m
-        else:print("It is already filled...enter another position..!")
+        if l[p-1]==' ':
+            l[p-1]=m
+        else:
+            while l[p-1]!=' ':
+                    p=int(input(f"It is already filled...enter another position to place {m}:"))
+                    if l[p-1]==' ':
+                        l[p-1]=m
+                        break
         if((l[0]==l[1]==l[2]=="X" or l[0]==l[1]==l[2]=="O")or(l[3]==l[4]==l[5]=="X" or l[3]==l[4]==l[5]=="O") or (l[6]==l[7]==l[8]=="X" or l[6]==l[7]==l[8]=="O") or (l[0]==l[3]==l[6]=="X" or l[0]==l[3]==l[6]=="O") or (l[1]==l[4]==l[7]=="X" or l[1]==l[4]==l[7]=="O") or (l[2]==l[5]==l[8]=="X" or l[2]==l[5]==l[8]=="O") or (l[0]==l[4]==l[8]=="X" or l[0]==l[4]==l[8]=="O") or (l[2]==l[4]==l[6]=="X" or l[2]==l[4]==l[6]=="O")):
             printboard(l)
             flag=True
